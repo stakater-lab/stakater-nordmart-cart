101d61d498ffb2fe317980d357d71d991f326885
Muhammad Hamza Zaib <hamza@aurorasolutions.io>
2019-05-30 14:37:24 +0500

    Do not add null istio headers.

--- src/main/java/com/stakater/nordmart/common/IstioHeaders.java
+++ src/main/java/com/stakater/nordmart/common/IstioHeaders.java
@@ -1,53 +1,84 @@
 package com.stakater.nordmart.common;
 
+import java.util.HashMap;
+import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 
 public class IstioHeaders
 {
-    public final String requestId;
-    public final String b3TraceId;
-    public final String b3SpanId;
-    public final String b3ParentSpanId;
-    public final String b3Sampled;
-    public final String b3Flags;
-    public final String otSpanId;
+    private final String requestId;
+    private final String b3TraceId;
+    private final String b3SpanId;
+    private final String b3ParentSpanId;
+    private final String b3Sampled;
+    private final String b3Flags;
+    private final String otSpanContext;
 
-    public IstioHeaders(HttpServletRequest request)
+    IstioHeaders(HttpServletRequest request)
     {
         if (request != null)
         {
-            this.requestId = request.getHeader("x-request-id") != null ?
-                request.getHeader("x-request-id") : "";
-
-            this.b3TraceId = request.getHeader("x-b3-traceid") != null ?
-                request.getHeader("x-b3-traceid") : "";
-
-            this.b3SpanId = request.getHeader("x-b3-spanid") != null ?
-                request.getHeader("x-b3-spanid") : "";
-
-            this.b3ParentSpanId = request.getHeader("x-b3-parentspanid") != null ?
-                request.getHeader("x-b3-parentspanid") : "";
-
-            this.b3Sampled = request.getHeader("x-b3-sampled") != null ?
-                request.getHeader("x-b3-sampled") : "";
-
-            this.b3Flags = request.getHeader("x-b3-flags") != null ?
-                request.getHeader("x-b3-flags") : "";
-
-            this.otSpanId = request.getHeader("x-ot-span-context") != null ?
-                request.getHeader("x-ot-span-context") : "";
+            this.requestId = request.getHeader("x-request-id");
+            this.b3TraceId = request.getHeader("x-b3-traceid");
+            this.b3SpanId = request.getHeader("x-b3-spanid");
+            this.b3ParentSpanId = request.getHeader("x-b3-parentspanid");
+            this.b3Sampled = request.getHeader("x-b3-sampled");
+            this.b3Flags = request.getHeader("x-b3-flags");
+            this.otSpanContext = request.getHeader("x-ot-span-context");
         }
         else
         {
-            this.requestId = "";
-            this.b3TraceId = "";
-            this.b3SpanId = "";
-            this.b3ParentSpanId = "";
-            this.b3Sampled = "";
-            this.b3Flags = "";
-            this.otSpanId = "";
+            this.requestId = null;
+            this.b3TraceId = null;
+            this.b3SpanId = null;
+            this.b3ParentSpanId = null;
+            this.b3Sampled = null;
+            this.b3Flags = null;
+            this.otSpanContext = null;
         }
     }
+
+    public Map<String, String> getHeaders()
+    {
+        Map<String, String> headers = new HashMap<>();
+        if (requestId != null)
+        {
+            headers.put("x-request-id", requestId);
+        }
+
+        if (b3TraceId != null)
+        {
+            headers.put("x-b3-traceid", b3TraceId);
+        }
+
+        if (b3SpanId != null)
+        {
+            headers.put("x-b3-spanid", b3SpanId);
+        }
+
+        if (b3ParentSpanId != null)
+        {
+            headers.put("x-b3-parentspanid", b3ParentSpanId);
+        }
+
+        if (b3Sampled != null)
+        {
+            headers.put("x-b3-sampled", b3Sampled);
+        }
+
+        if (b3Flags != null)
+        {
+            headers.put("x-request-id", b3Flags);
+        }
+
+        if (otSpanContext != null)
+        {
+            headers.put("x-ot-span-context", otSpanContext);
+        }
+
+        return headers;
+    }
+
 
     @Override
     public String toString()
@@ -59,7 +90,7 @@ public class IstioHeaders
             ", b3ParentSpanId='" + b3ParentSpanId + '\'' +
             ", b3Sampled='" + b3Sampled + '\'' +
             ", b3Flags='" + b3Flags + '\'' +
-            ", otSpanId='" + otSpanId + '\'' +
+            ", otSpanContext='" + otSpanContext + '\'' +
             '}';
     }
 }
--- src/main/java/com/stakater/nordmart/common/Utils.java
+++ src/main/java/com/stakater/nordmart/common/Utils.java
@@ -1,5 +1,6 @@
 package com.stakater.nordmart.common;
 
+import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.web.context.request.RequestAttributes;
 import org.springframework.web.context.request.RequestContextHolder;
@@ -7,10 +8,17 @@ import org.springframework.web.context.request.ServletRequestAttributes;
 
 public class Utils
 {
-    public static HttpServletRequest getCurrentHttpRequest(){
+    public static Map<String, String> getIstioHeaders()
+    {
+        return new IstioHeaders(getCurrentHttpRequest()).getHeaders();
+    }
+
+    private static HttpServletRequest getCurrentHttpRequest()
+    {
         RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();
-        if (requestAttributes instanceof ServletRequestAttributes) {
-            return ((ServletRequestAttributes)requestAttributes).getRequest();
+        if (requestAttributes instanceof ServletRequestAttributes)
+        {
+            return ((ServletRequestAttributes) requestAttributes).getRequest();
         }
         return null;
     }
